9118e0dad0c03daebb31f61d4ac84695079efdfe
Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
2022-04-07 08:38:56 +0530

    Refund and Cancellation

--- pages/index.js
+++ pages/index.js
@@ -6,38 +6,6 @@ import Header from "../components/Header/Header";
 import LiveCourse from "../components/LiveCourse";
 
 export default function Home() {
-  var sapCourseList = [
-    { name: "SAP BPC", image: "/images/sap/bpc.png" },
-    // {name:"SAP EWM", image: "/images/sap/"},
-    // {name:"SAP FICO", image: "/images/sap/"},
-    // {name:"SAP MM (Materials Management)", image: "/images/sap/"},
-    { name: "SAP PM (Plant Maintenance)", image: "/images/sap/pm.png" },
-    // {name:"SAP PP (Production Planning)", image: "/images/sap/"},
-    { name: "SAP QM (Quality Management)", image: "/images/sap/qm.png" },
-    { name: "SAP S/4HANA Logistics", image: "/images/sap/s4hana.png" },
-    { name: "SAP SD (Sales and Distribution)", image: "/images/sap/sd.png" },
-    {
-      name: "SAP SuccessFactors Employee Central",
-      image: "/images/sap/sf.png",
-    },
-    {
-      name: "SAP SuccessFactors Recruiting Management (RCM)",
-      image: "/images/sap/sfrcm.png",
-    },
-    {
-      name: "SAP TRM (Treasury and Risk Management)",
-      image: "/images/sap/trm.png",
-    },
-    { name: "SAP WM (Warehouse Management)", image: "/images/sap/wm.png" },
-  ];
-
-  var oracleCourseList = [
-    { name: "Oracle APEX", image: "/images/oracle/apex.png" },
-    { name: "Oracle BI Publisher", image: "/images/oracle/bi_pub.png" },
-    { name: "Oracle Forms", image: "/images/oracle/forms.png" },
-    { name: "Oracle Argus", image: "/images/oracle/argus.png" },
-    { name: "Oracle Documaker", image: "/images/oracle/documaker.png" },
-  ];
   return (
     <div>
       <Head>
---
+++ pages/refund-and-cancellation.js
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import React from "react";
+import Header from "../components/Header/Header";
+import classes from "../styles/pages/refunds.module.scss";
+
+export default function RefundAndCancellation() {
+  return (
+    <div>
+      <Head>
+        <title>Infodal</title>
+        <meta
+          name="description"
+          content="Infodal Learning- India's No.1 IT Learning Platform"
+        />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+
+      <header>
+        <Header />
+      </header>
+
+      <div className="p-4 lg:px-40">
+        <h1 className="text-2xl font-semibold">
+          Refund and Cancellation Policy
+        </h1>
+        <h2 className={classes.subheading}>Overview</h2>
+        <p className={classes.paragraph}>
+          {
+            'This website is operated by Infodal Trainings Pvt Ltd. Throughout the site, the terms "we", "us" and "our" refer to Pazit Infotech Limited. Pazit Infotech limited offers this website, including all information, tools and services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here'
+          }
+        </p>
+        <h2 className="text-black text-2xl mt-5 font-semibold">
+          Refund & Cancellation
+        </h2>
+        <p className={classes.paragraph}>
+          Please refer to the refund policy below:
+        </p>
+        <h2 className={classes.subheading}>Instructor-led Online Course</h2>
+        <ul className={classes.list}>
+          <li>
+            No Refund Policy - Live instructor-led course fees is
+            non-refundable.
+          </li>
+        </ul>
+        <h2 className={classes.subheading}>Video Course</h2>
+        <ul className={classes.list}>
+          <li>
+            Refund Policy - Once a video course has been purchased, we will
+            issue a refund into the back account provided, only within 7 days
+            from the date of purchase,. Kindly watch the preview video of the
+            course you want to purchase or talk to our team if you have any
+            queries before buying the course.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
