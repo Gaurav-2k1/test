55bd38ed22a0034b19c129a008a7aab039d3387b
Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
2022-04-23 21:11:32 +0530

    Changes

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -15,9 +15,8 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { headerPaths } from "../../config/config";
 import { theme } from "../../utils/theme";
-import CallIcon from '@mui/icons-material/Call';
-import EmailIcon from '@mui/icons-material/Email';
-
+import CallIcon from "@mui/icons-material/Call";
+import EmailIcon from "@mui/icons-material/Email";
 
 export default function Header() {
   const isDesktop = useMediaQuery(theme.breakpoints.up("sm"));
@@ -71,8 +70,8 @@ export default function Header() {
           </IconButton>
         </div>
       </div>
-      <div className="lg:flex  flex-row gap-40 justify-around">
-        <div className="hidden lg:flex relative w-24 h-24  lg:mt-2 lg:ml-2 lg:w-30 lg:h-30 ">
+      <div className="flex flex-row justify-evenly">
+        <div className="hidden lg:flex relative w-24 h-24 lg:w-30 lg:h-30 ">
           <Image
             className="lg:w-30"
             src="/logo.png"
@@ -81,8 +80,8 @@ export default function Header() {
             objectFit="scale-down"
           />
         </div>
-        <div className="hidden lg:flex  lg:self-center ">
- <div className="text-base  font-bold mt-2 ml-5 lg:justify-items-center  ">
+        <div className="hidden lg:flex lg:self-center ">
+          <div className="text-base  font-bold mt-2 ml-5 lg:justify-items-center  ">
             <CallIcon className="text-3xl " />
             +44 7454 666010
             <br />
@@ -90,16 +89,18 @@ export default function Header() {
               <CallIcon className="text-3xl" />
               +91 93025 84342
             </div>
- </div>
-          <div className="text-base font-bold lg:self-center  lg:ml-10"> <EmailIcon className="text-3xl" />contact@infodal.com</div>
-          <div className="lg:self-center" >
-          <Button className="bg-orange-500 underline hover:bg-red-500 hover:no-underline text-white font-medium text-base lg:px-7 lg:py-1 rounded-sm  lg:ml-12 ">
-              Register
-            </Button>
-            
+          </div>
+          <div className="text-base font-bold lg:self-center lg:ml-10">
+            <EmailIcon className="text-3xl" />
+            contact@infodal.com
           </div>
           <div className="lg:self-center">
-          <Button className="bg-green-500 underline hover:bg-red-500 hover:no-underline text-white font-medium text-base lg:px-7 lg:py-1 rounded-sm  lg:ml-12">
+            <Button className="bg-orange-500 underline hover:bg-red-500 hover:no-underline text-white font-medium text-base lg:px-7 lg:py-1 rounded-sm  lg:ml-12 ">
+              Register
+            </Button>
+          </div>
+          <div className="lg:self-center">
+            <Button className="bg-green-500 underline hover:bg-red-500 hover:no-underline text-white font-medium text-base lg:px-7 lg:py-1 rounded-sm  lg:ml-12">
               Sign In
             </Button>
           </div>
@@ -107,7 +108,7 @@ export default function Header() {
       </div>
       <div className=" lg:bg-blue-700	">
         {isDesktop ? (
-          <div className="w-2/2  self-center  text-white font-normal   flex flex-rows-4  lg:w-15  justify-evenly display:block m-auto text-sm ">
+          <div className="w-full self-center  text-white font-normal   flex flex-rows-4  lg:w-15  justify-evenly display:block m-auto text-sm ">
             {headerPaths.map((headerPath) => (
               <HeaderItem
                 key={headerPath.name}
@@ -117,37 +118,28 @@ export default function Header() {
             ))}
           </div>
         ) : (
-         <>
+          <>
+            <div className="flex flex-row w-full justify-between">
+              <div className="relative w-24 h-24 ml-5 lg:self-center lg:ml-[15vh]">
+                <Image
+                  className="lg:w-1/3"
+                  src="/logo.png"
+                  alt="Logo"
+                  layout="fill"
+                  objectFit="scale-down"
+                />
+              </div>
 
-         <div className="flex flex-row w-full justify-between">
-           <div className="relative w-24 h-24 ml-5 lg:self-center lg:ml-[15vh]">
-              <Image
-            className="lg:w-1/3"
-            src="/logo.png"
-            alt="Logo"
-            layout="fill"
-            objectFit="scale-down"
-          /></div>
-         
-           
-            <div className="relative self-center ">
-            <IconButton size="large" onClick={() => toggleDrawer(true)}>
-              <Menu />
-            </IconButton>
+              <div className="relative self-center ">
+                <IconButton size="large" onClick={() => toggleDrawer(true)}>
+                  <Menu />
+                </IconButton>
+              </div>
             </div>
-            
-          
-        
-
-         </div>
-        
-        
-         </>
-         
+          </>
         )}
       </div>
       <div className="z-50">
-
         <SwipeableDrawer
           anchor="right"
           open={isOpen}
@@ -155,58 +147,48 @@ export default function Header() {
           onClose={() => toggleDrawer(false)}
           className=""
         >
-
-          <div className="text-center text-2xl  h-full">
-
+          <div className="text-center text-2xl h-full">
             <div className="flex flex-col justify-evenly h-[60vh]">
-
               {headerPaths.map((headerPath) => (
                 <>
-
                   <HeaderItem
                     key={headerPath.path}
                     name={headerPath.name}
                     route={headerPath.path}
-
                   />
 
-
                   <Divider />
-
                 </>
               ))}
-              
             </div>
-            <div className=" lg:flex flex-row gap-4 ">
-               
-                <div className="text-base  font-bold mt-2 ml-5 lg:ml-12  ">
+            <div className=" lg:flex flex-row gap-4  p-4">
+              <div className="text-base  font-bold mt-2 ml-5 lg:ml-12  ">
+                <CallIcon className="text-3xl" />
+                +44 7454 666010
+                <br />
+                <div className="lg:mt-2">
                   <CallIcon className="text-3xl" />
-                  +44 7454 666010
-                  <br />
-                  <div className="lg:mt-2">
-                    <CallIcon className="text-3xl" />
-                    +91 93025 84342
-                  </div>
-
-                </div>
-                <div className="text-base font-bold  lg:mt-8 lg:ml-10"> <EmailIcon className="text-3xl" />contact@infodal.com</div>
-                <div >
-                <Button className="bg-orange-500 px-2 py-2 w-30 h-10 underline hover:bg-red-500 hover:no-underline text-white font-medium text-base lg:px-7 lg:py-1 rounded-sm  lg:ml-12 lg:mt-8">
-                    Register
-                  </Button>
-                </div>
-                <div>
-
-                <Button className="bg-green-500 mt-3 px-3 py-2 w-30 h-10 underline hover:bg-red-500 hover:no-underline text-white font-medium text-base lg:px-7 lg:py-1 rounded-sm  lg:ml-12 lg:mt-8">
-                    Sign In
-                  </Button>
-                    
-                  
+                  +91 93025 84342
                 </div>
               </div>
+              <div className="text-base font-bold  lg:mt-8 lg:ml-10">
+                {" "}
+                <EmailIcon className="text-3xl" />
+                contact@infodal.com
+              </div>
+              <div>
+                <Button className="bg-orange-500 px-2 py-2 w-30 h-10 underline hover:bg-red-500 hover:no-underline text-white font-medium text-base lg:px-7 lg:py-1 rounded-sm  lg:ml-12 lg:mt-8">
+                  Register
+                </Button>
+              </div>
+              <div>
+                <Button className="bg-green-500 mt-3 px-3 py-2 w-30 h-10 underline hover:bg-red-500 hover:no-underline text-white font-medium text-base lg:px-7 lg:py-1 rounded-sm  lg:ml-12 lg:mt-8">
+                  Sign In
+                </Button>
+              </div>
+            </div>
           </div>
         </SwipeableDrawer>
-
       </div>
     </div>
   );
@@ -220,8 +202,8 @@ export function HeaderItem({ name, route }) {
 
   return (
     <h1
-      className="cursor-pointer hover:bg-red-500 underline-offset-8  lg:pt-3 lg:h-12 lg:pl-2 lg:pr-2 lg:border-x-2"
-      onClick ={onClickHandler}
+      className="cursor-pointer underline-offset-8  lg:pt-3 lg:h-12 lg:pl-2 lg:pr-2 lg:border-x-2"
+      onClick={onClickHandler}
     >
       {name}
     </h1>
