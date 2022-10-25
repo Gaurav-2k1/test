992ac837df1b7f6af7c244dbfb181d05be052e57
Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
2022-10-25 20:59:33 +0530

    Become an Instructor

---
+++ pages/become-an-instructor/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BecomeInstructor from "../../components/Body/BecomeInstructor";
+import InstructorForm from "../../components/Forms/InstructorForm";
+export default function BecomeAnInstructor() {
+  return (
+    <>
+      <BecomeInstructor></BecomeInstructor>
+      <InstructorForm />
+    </>
+  );
+}
