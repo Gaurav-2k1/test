9930f73147129ac0a74a043deed35fd73c601014
Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
2022-11-11 19:20:36 +0530

    Change Infodal URL

--- constants/constants.js
+++ constants/constants.js
@@ -7,4 +7,4 @@ export const razorpay_key_id = "rzp_live_4uH7JzK5nuNg86";
 
 // export const DEPLOYMENT_URL = "http://localhost:3000";
 // export const DEPLOYMENT_URL = "http://192.168.0.109:3000";
-export const DEPLOYMENT_URL = "https://infodal-ui-chandiwalaaadhar.vercel.app";
+export const DEPLOYMENT_URL = "https://infodal.com";
