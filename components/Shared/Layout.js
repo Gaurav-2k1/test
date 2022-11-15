729800369591d49f18c5b8ef29faf4a900a4e5cc
Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
2022-11-15 20:08:38 +0530

    Added Microsoft Clarity

--- components/Shared/Layout.js
+++ components/Shared/Layout.js
@@ -22,6 +22,13 @@ export default function Layout({ children }) {
       </Head>
 
       <Header />
+      <Script id="clarity" type="text/javascript" strategy="afterInteractive">
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ejduxsdgl0");`}
+      </Script>
       <Script
         type="text/javascript"
         id="hs-script-loader"
