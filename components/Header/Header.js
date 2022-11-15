d58f781f00de588fc3a51e7a8c4d414b5b2f9baf
Tharunkumar001 <tharung1411@gmail.com>
2022-11-15 12:37:38 +0530

    Navbar ui update

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,6 @@
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import { useMediaQuery } from "@mui/material";
+import { Button, IconButton, useMediaQuery } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getMenuToggle, setMenuToggle } from "../../store/modalSlice";
 import MenuBar from "./MenuBar";
 import Link from "next/link";
+import { ArrowDropDown, MoreVertOutlined } from "@mui/icons-material";
 
 export default function Header() {
   const isMenuOpen = useSelector(getMenuToggle);
@@ -19,26 +20,25 @@ export default function Header() {
   };
 
   return (
-    <div className="sticky top-0 left-0 right-0 bg-white z-30 shadow-md">
-      <div className="flex flex-row justify-between py-2 px-2 items-center">
-        <div className="w-20">
-          <MenuIcon
-            fontSize="large"
-            className="justify-self-start"
-            onClick={toggleDrawer}
-          />
-          <MenuBar />
+    <div className="sticky top-0 left-0 right-0 bg-white z-30 shadow-md flex flex-row ">
+      <div className="flex flex-row py-2 px-2 items-center">
+        <div className="w-20 h-fit flex flex-row pl-4">
+            <Image src={Logo} alt="Infodal Logo" />  
         </div>
-        <Link href="/" passHref>
-          <div className="w-20 h-fit">
-            <Image src={Logo} alt="Infodal Logo" />
-          </div>
-        </Link>
-        <div className="w-20 flex flex-row justify-end">
-          <Link href="/search?query=" passHref>
-            <SearchOutlinedIcon />
-          </Link>
-          {/* <ShoppingCartOutlinedIcon /> */}
+        <div className="w-25 h-fit flex flex-row px-8">
+          <button id="dropdownDefault" data-dropdown-toggle="dropdown" className="text-white bg-primary px-8 rounded text-base font-bold focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 py-2.5 text-center inline-flex items-center" type="button">EXPLORE <svg className="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
+        </div>
+      </div>
+      
+      <div className="flex flex-row py-2 px-2 items-center absolute top-0 right-0 px-4">
+        <div className="w-25 h-fit flex flex-row px-4">
+          <button id="dropdownDefault" data-dropdown-toggle="dropdown" className="text-primary rounded font-bold px-4 py-2.5" type="button">LOGIN</button>
+        </div>
+        <div className="w-25 h-fit flex flex-row px-4">
+          <button id="dropdownDefault" data-dropdown-toggle="dropdown" className="text-white rounded w-32 text-center font-bold bg-primary px-4 py-2.5" type="button">SIGN UP</button>
+        </div>
+        <div className="w-25 h-fit flex flex-row px-4 rounded">
+          <button id="dropdownDefault" data-dropdown-toggle="dropdown" className="text-black border-2 border-indigo-500/100 rounded font-bold px-4 py-2.5 text-center inline-flex items-center" type="button">INDIAN RUPEES - INR</button>
         </div>
       </div>
     </div>
