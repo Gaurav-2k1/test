3946e5d9ccdf01e98892f5906de46e629de08a4a
Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
2022-12-08 15:47:30 +0530

    Analyze

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["infodalstorage.blob.core.windows.net"],
   },
 };
-module.exports = withBundleAnalyzer({});
+
+module.exports = (phase, defaultConfig) => {
+  return withBundleAnalyzer(defaultConfig);
+};
 module.exports = nextConfig;
